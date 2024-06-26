fbef40d77dbc330803dd19c53e58d07d09815e3e
LoveCodeYouth <q1978744363@qq.com>
2024-06-26 12:23:03 -0400

    Site updated: 2024-06-26 12:23:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/25/Linux启程/","2024/06/25/hello-world/"];function toRandomPost(){
+var posts=["2024/06/25/Linux启程/","2024/06/25/hello-world/","2024/06/26/hexo项目常用指令/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
